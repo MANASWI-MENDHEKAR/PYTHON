06cce9468362657b436b14cbc8e651d9b8953c3e
MANASWI-MENDHEKAR <154888424+MANASWI-MENDHEKAR@users.noreply.github.com>
2024-04-18 21:58:15 +0530

    Add files via upload

---
+++ exp2(DICTIONARY , SET ).py
@@ -0,0 +1,17 @@
+# CREATION OF DICTIONARY
+
+phones = {
+    "ram" : 65,
+    "shyam":78,
+    "mohan":87
+
+}
+
+print(phones)
+
+
+
+#CREATION OF SET
+
+names = {"manaswi", "sakshi" , " mansi"}
+print(names)
---
+++ exp2(STRING).py
@@ -0,0 +1,25 @@
+#CREATION OF STRING
+name= input("enter your name :")
+print("welcome",name)
+
+#CONCATINATION
+
+a= " hey "
+b=" how are you ?"
+print(a + b)
+
+
+#SLICING
+
+s= " hello "
+print(s[1:4])
+
+#INDEXING
+
+s = "hello"
+print(s[4])
+
+#REPETITION
+
+s=" hello "
+print(s*3)
