f05665fb797acd6a4bfc8d0b4d79d65d26fa4a4c
MANASWI-MENDHEKAR <154888424+MANASWI-MENDHEKAR@users.noreply.github.com>
2024-04-20 21:33:31 +0530

    Update exp2(TUPLE).py

--- exp2(TUPLE).py
+++ exp2(TUPLE).py
@@ -28,28 +28,3 @@ print(l*3)
 
 
 
-#CREATION OF DICTIONARY
-l={45,67,75,76,765,"hi","blah"}
-print(l)
-
-#INDEXING OF DICTIONARY
-
-l={45,67,75,76,765,"hi","blah"}
-print(KEY=2)
-
-#SLICING OF DICTIONARY
-
-l={45,67,75,76,765,"hi","blah"}
-print(l[1:5])
-
-#CONCATINATION
-
-l={45,67,75,76,765,"hi","blah"}
-m={32.45,65,7,876,653,"haha","gdh"}
-print(l+m)
-
-#REPETITION
-
-l={45,67,75,76,765,"hi","blah"}
-#m=(32.45,65,7,876,653,"haha","gdh")
-print(l*3)
