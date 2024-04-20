1a8e41dd291da5634e581c40072db3507c462701
MANASWI-MENDHEKAR <154888424+MANASWI-MENDHEKAR@users.noreply.github.com>
2024-04-20 21:30:58 +0530

    Update exp2(LIST).py

---
+++ exp2(LIST).py
@@ -0,0 +1,25 @@
+# CREATION OF LIST
+
+list=[1,3,5,2,7,5,8,"apple"]
+print(list)
+
+# INDEXING OF LIST
+
+colours = ["red", "blue", "green", "orange","pink"]
+print(colours[2])
+
+# SLICING OF LIST
+
+colours = ["red", "blue", "green", "orange","pink"]
+print(colours[1:4])
+
+# CONCATINATION OF LIST
+
+colours = ["red", "blue", "green", "orange","pink"]
+fruits = ["apple","berry","jaam","santra","lichi"]
+print(colours+fruits)
+
+# REPETITON OF LIST
+
+colours = ["red", "blue", "green", "orange","pink"]
+print(colours*3)
